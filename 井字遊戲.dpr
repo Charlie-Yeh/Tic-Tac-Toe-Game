52a3047d54bbfa6511777884e0a01f222d26942f
Charlie-Yeh <145676141+Charlie-Yeh@users.noreply.github.com>
2024-03-11 14:11:47 +0800

    Add files via upload

---
+++ 井字遊戲.dpr
@@ -0,0 +1,17 @@
+program ¤«¦r¹CÀ¸;
+
+uses
+  Vcl.Forms,
+  main in 'main.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows11 Impressive Dark');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
